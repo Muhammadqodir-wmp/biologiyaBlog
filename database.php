ebd24f0d3e3ad766db3b9fad90d857d1bea29954
Muhammadqodir <136580277+BAJARUVCHI@users.noreply.github.com>
2024-10-20 20:19:10 +0500

    Update database.php
    server almashdi

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
+$servername = "fdb1030.awardspace.net";
+$username = "4542046";
 $password = "";
-$database = "phpBlog";
+$database = "4542046_phpblog";
 try {
   $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
   // set the PDO error mode to exception
